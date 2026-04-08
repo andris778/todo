222bee0f816b6f1757397c315307f4dbedd622f4
acer e5-774g <atileckis@gmail.com>
2026-04-08 10:59:53 +0300

    statiskas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/why', function () {
+    return view('why');
+});
+
+Route::get('/home', function () {
+    return view('why');
+});
+
+Route::get('/lore', function () {
+    return view('why');
+});
+
+Route::get('/information', function () {
+    return view('why');
+});
+
+Route::get('/calendar', function () {
+    return view('why');
+});
